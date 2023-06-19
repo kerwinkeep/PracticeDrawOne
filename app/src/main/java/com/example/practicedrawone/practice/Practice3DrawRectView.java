519755a3435f4b8b2af32220ab54e4152e13105c
kerwinkeep <kerwinkeep@outlook.com>
2023-06-20 00:14:31 +0800

    完成练习内容：使用 canvas.drawRect() 方法画矩形

--- app/src/main/java/com/example/practicedrawone/practice/Practice3DrawRectView.java
+++ app/src/main/java/com/example/practicedrawone/practice/Practice3DrawRectView.java
@@ -2,6 +2,8 @@ package com.example.practicedrawone.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,16 +11,23 @@ import androidx.annotation.Nullable;
 
 public class Practice3DrawRectView extends View {
 
+    private Paint mRectPaint;
+
     public Practice3DrawRectView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public Practice3DrawRectView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mRectPaint = new Paint();
     }
 
     @Override
@@ -26,5 +35,7 @@ public class Practice3DrawRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRect() 方法画矩形
+        float baseLength = getWidth() / 3f;
+        canvas.drawRect(baseLength, baseLength, baseLength + baseLength, baseLength + baseLength, mRectPaint);
     }
 }
