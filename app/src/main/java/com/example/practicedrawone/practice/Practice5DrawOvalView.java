cea08a6f16c22f5b1ce548b58b536dbb06f015d2
kerwinkeep <kerwinkeep@outlook.com>
2023-06-20 00:45:32 +0800

    完成练习内容：使用 canvas.drawOval() 方法画椭圆

--- app/src/main/java/com/example/practicedrawone/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/example/practicedrawone/practice/Practice5DrawOvalView.java
@@ -2,6 +2,7 @@ package com.example.practicedrawone.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,16 +10,23 @@ import androidx.annotation.Nullable;
 
 public class Practice5DrawOvalView extends View {
 
+    private Paint mOvalPaint;
+
     public Practice5DrawOvalView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Practice5DrawOvalView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public Practice5DrawOvalView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mOvalPaint = new Paint();
     }
 
     @Override
@@ -26,5 +34,8 @@ public class Practice5DrawOvalView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawOval() 方法画椭圆
+        float baseLengthX = getWidth() / 3f;
+        int baseLengthY = getHeight() / 5;
+        canvas.drawOval(baseLengthX, baseLengthY * 2, baseLengthX * 2, baseLengthY * 3, mOvalPaint);
     }
 }
