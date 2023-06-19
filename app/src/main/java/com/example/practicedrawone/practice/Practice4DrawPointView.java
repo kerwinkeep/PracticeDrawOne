53f13210489c77c951b38733d14d15970edabfea
kerwinkeep <kerwinkeep@outlook.com>
2023-06-20 00:39:01 +0800

    完成练习内容：使用 canvas.drawPoint() 方法画点

--- app/src/main/java/com/example/practicedrawone/practice/Practice4DrawPointView.java
+++ app/src/main/java/com/example/practicedrawone/practice/Practice4DrawPointView.java
@@ -2,6 +2,7 @@ package com.example.practicedrawone.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,16 +10,29 @@ import androidx.annotation.Nullable;
 
 public class Practice4DrawPointView extends View {
 
+    private Paint mPointPaint;
+
     public Practice4DrawPointView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public Practice4DrawPointView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public Practice4DrawPointView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPointPaint = new Paint();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mPointPaint.setStrokeWidth(Math.min(w, h) * 0.1f);
     }
 
     @Override
@@ -28,5 +42,11 @@ public class Practice4DrawPointView extends View {
 //        练习内容：使用 canvas.drawPoint() 方法画点
 //        一个圆点，一个方点
 //        圆点和方点的切换使用 paint.setStrokeCap(cap)：`ROUND` 是圆点，`BUTT` 或 `SQUARE` 是方点
+        float baseLengthX = getWidth() / 3f;
+        float y = getHeight() >>> 1;
+        mPointPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvas.drawPoint(baseLengthX, y, mPointPaint);
+        mPointPaint.setStrokeCap(Paint.Cap.SQUARE);
+        canvas.drawPoint(baseLengthX + baseLengthX, y, mPointPaint);
     }
 }
